03fe70ee8d1d8813098be21956f3daca0d92dcd0
Andrés <andresrf53@educastur.es>
2026-06-21 23:35:28 +0200

    Plane lights

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,24 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
   //First project with external LED and breadboard.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(200);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(500);
+  digitalWrite(13,HIGH);
   delay(50);
   digitalWrite(13, LOW);
-  delay(100);
-  digitalWrite(13, HIGH);
-  delay(50);
-  digitalWrite(13, LOW);
-  delay(1000);
+  delay(500);
 
 }
 
