3d22c00384f69e46805e842361bc3574283688e6
Andrés <andresrf53@educastur.es>
2026-06-21 23:26:47 +0200

    First external LED

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,20 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  //First project with external LED and breadboard.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
-  delay(100);
+  delay(50);
   digitalWrite(13, LOW);
   delay(100);
   digitalWrite(13, HIGH);
-  delay(100);
+  delay(50);
   digitalWrite(13, LOW);
   delay(1000);
+
 }
 
 // put function definitions here:
