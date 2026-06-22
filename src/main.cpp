9990781201487504967a58b5c73a0f6ecc71e228
Andrés <andresrf53@educastur.es>
2026-06-22 14:35:01 +0200

    Variables + SOS LED

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,59 @@
 
 // put function declarations here:
 //int myFunction(int, int);
+int redLed = 4;
+float pi = 3.14;
+int dit = 250;
+int dah = 700;
+int LongWait = 2000;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(redLed, OUTPUT);
   //First project with external LED and breadboard.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(12, HIGH);
-  delay(50);
-  digitalWrite(12, LOW);
-  delay(200);
-  digitalWrite(12, HIGH);
-  delay(50);
-  digitalWrite(12, LOW);
-  delay(500);
-  digitalWrite(13,HIGH);
-  delay(50);
-  digitalWrite(13, LOW);
-  delay(500);
-
+  //S
+  digitalWrite(redLed, HIGH);
+  delay(dit);
+  digitalWrite(redLed,LOW);
+  delay(dit);
+  digitalWrite(redLed,HIGH);
+  delay(dit);
+  digitalWrite(redLed, LOW);
+  delay(dit);
+  digitalWrite(redLed, HIGH);
+  delay(dit);
+  digitalWrite(redLed, LOW);
+  delay(dit);
+  //O
+  digitalWrite(redLed, HIGH);
+  delay(dah);
+  digitalWrite(redLed, LOW);
+  delay(dah);
+  digitalWrite(redLed,HIGH);
+  delay(dah);
+  digitalWrite(redLed, LOW);
+  delay(dah);
+  digitalWrite(redLed,HIGH);
+  delay(dah);
+  digitalWrite(redLed,LOW);
+  delay(dah);
+  //S
+  digitalWrite(redLed, HIGH);
+  delay(dit);
+  digitalWrite(redLed,LOW);
+  delay(dit);
+  digitalWrite(redLed,HIGH);
+  delay(dit);
+  digitalWrite(redLed, LOW);
+  delay(dit);
+  digitalWrite(redLed, HIGH);
+  delay(dit);
+  digitalWrite(redLed, LOW);
+  delay(LongWait);
 }
 
 // put function definitions here:
