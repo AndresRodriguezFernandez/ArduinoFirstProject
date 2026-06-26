082d77b0df06f8483b4b373438d42596587fc9bb
Andrés <andresrf53@educastur.es>
2026-06-26 23:44:11 +0200

    Alarm + 2 LED's

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
 
 const int pinBliz = 8;
-const int pinLED = 13; 
+const int pinLED1 = 13;
+const int pinLED2 = 12; 
 
 void setup() {
   pinMode(pinBliz, OUTPUT);
-  pinMode(pinLED, OUTPUT);
+  pinMode(pinLED1, OUTPUT);
+  pinMode(pinLED2, OUTPUT);
 }
 
 void loop() {
   tone(pinBliz, 1400);
-  digitalWrite(pinLED, HIGH); 
+  digitalWrite(pinLED1, HIGH); 
   delay(400); 
-
+  digitalWrite(pinLED1, LOW);
   tone(pinBliz, 1000); 
-  digitalWrite(pinLED, LOW); 
+  digitalWrite(pinLED2, HIGH);
   delay(400); 
+  digitalWrite(pinLED2, LOW);
 
 }
